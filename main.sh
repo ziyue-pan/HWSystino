da045e30af6fb95cd65d2f6fd316fa951eccc3ed
线上红色筑梦--暑期社会实践团 <alphavake2018@gmail.com>
2020-08-12 10:11:02 +0800

    n

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # 临时变量声明
-zenity_size=" --height 160 --width 320 "
-temp1=$(mktemp -t temp1.XXXXXX)
+error_size=" --height 160 --width 320 "
+list_size=" --height 640 --width 480 "
 
 # 前向函数声明
 function main() {
@@ -21,27 +21,28 @@ function main() {
 
         if [ $? -eq 0 ]; then
             mysql_info=" -u$mysql_username -p$mysql_password"
+            mysql_default=" mysql -N HW -u $mysql_username -p$mysql_password "
             break
         else
-            zenity --error --title "Wrong Username or Password" $zenity_size \
-                --text "Cannot connect to your MySQL system. Please try again."
+            Err "Wrong Username or Password" \
+                "Cannot connect to your MySQL system. Please try again."
         fi
     done
 
     Initial
+
     while [ 1 ]; do
 
-        zenity --list --title "Choose Your Identity" \
+        selection=$(zenity --list --title "Choose Your Identity" \
+            --cancel-label Quit \
             --column "Identity" \
             "Administrator" \
             "Teacher" \
-            "Student" >$temp1
+            "Student")
 
         if [ $? -eq 1 ]; then
             break
         fi
-
-        selection=$(cat $temp1)
 
         case $selection in
         "Administrator")
@@ -71,14 +72,14 @@ function Initial() {
         if [ $? -eq 0 ]; then
             echo "initialization done!"
         else
-            zenity --error --title "Fail to Initialize Database" \
-                $zenity_size --text "Please check your init.sql script."
+            Err "Fail to Initialize Database" \
+                "Please check your init.sql script."
             exit 1
         fi
 
     else
-        zenity --error --title "Fail to Initialize Database" \
-            $zenity_size --text "Cannot find file: $init_file"
+        Err "Fail to Initialize Database" \
+            "Cannot find file: $init_file"
         exit 1
     fi
 
@@ -95,9 +96,10 @@ function LoginAdmin() {
 
         if [ $result -eq 1 ]; then
             echo "successfully login in as administrator"
+            DisplayAdminMenu
         else
-            zenity --error --title "Fail to Login in as Administrator" \
-                $zenity_size --text "Wrong matches! Please re-check your username or password"
+            Err "Fail to Login in as Administrator" \
+                "Wrong matches! Please re-check your username or password"
         fi
     fi
 }
@@ -114,8 +116,8 @@ function LoginTeacher() {
         if [ $result -eq 1 ]; then
             echo "successfully login in as teacher"
         else
-            zenity --error --title "Fail to Login in as Teacher" \
-                $zenity_size --text "Wrong matches! Please re-check your username or password"
+            Err "Fail to Login in as Teacher" \
+                "Wrong matches! Please re-check your username or password"
         fi
     fi
 }
@@ -132,10 +134,107 @@ function LoginStudent() {
         if [ $result -eq 1 ]; then
             echo "successfully login in as student"
         else
-            zenity --error --title "Fail to Login in as Student" \
-                $zenity_size --text "Wrong matches! Please re-check your username or password"
+            Err "Fail to Login in as Student" \
+                "Wrong matches! Please re-check your username or password"
         fi
     fi
+}
+
+function DisplayAdminMenu() {
+
+    while [ 1 ]; do
+
+        selection=$(zenity --list --title "Choose Your Operation" \
+            --column "Operation" \
+            "Manage Teacher Accounts" \
+            "Manage Courses" \
+            "Manage Teaching Affairs" \
+            "* Back")
+
+        if [ $? -eq 1 ]; then
+            break
+        fi
+
+        case $selection in
+        "Manage Teacher Accounts")
+            ManageTeacherAccounts
+            ;;
+
+        "Manage Courses")
+            ManageCourses
+            ;;
+
+        "Manage Teaching Affairs")
+            ManageTeachingAffairs
+            ;;
+        *)
+            break
+            ;;
+        esac
+    done
+}
+
+function DisplayTeacherMenu() {
+    echo "DisplayTeacherMenu"
+}
+
+function DisplayStudentMenu() {
+    echo "DisplayTeacherMenu"
+}
+
+function ManageTeacherAccounts() {
+    while [ 1 ]; do
+        selection=$(echo "SELECT * FROM teacher" | $mysql_default |
+            tr '\t' '\n' | zenity --list --title "Teacher Accounts" \
+            --text "" --column "Teacher ID" --column "Password" \
+            --column "Name" $list_size --extra-button "Add" \
+            --extra-button "Delete" --ok-label "Modify")
+
+        if [ $? -eq 1 ]; then
+            if [[ $selection = "Delete" ]]; then
+                echo "delete account"
+            elif [[ $selection = "Add" ]]; then
+                echo "add account"
+            else
+                break
+            fi
+        else
+            if [[ -z $selection ]]; then
+                Err "Wrong selection" "Must select an account"
+            else
+                echo $selection
+            fi
+
+        fi
+    done
+}
+
+function ManageCourses() {
+    echo "ManageCourses"
+}
+
+function ManageTeachingAffairs() {
+    echo "ManageTeachingAffairs"
+}
+
+function ManageStudentAccounts() {
+    echo "ManageStudentAccounts"
+}
+
+function ManageInformation() {
+    echo "ManageInformation"
+}
+
+function ManageHomework() {
+    echo "ManageHomework"
+}
+
+function ManageStatus() {
+    echo "ManageStatus"
+}
+
+function Err() {
+    zenity --error --title "$1" $error_size --text "$2"
 }
 
 # 主函数调用
